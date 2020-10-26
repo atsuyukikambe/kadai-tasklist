abf51807b63aba0e953d145695197098578a6aec
atsuyuki kambe <kanbe7767@gmail.com>
2020-10-26 07:40:37 +0000

    kadai-tasklist

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 // CRUD
+// create: 新規作成用のフォームページ
+Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
 // メッセージの個別詳細ページ表示
 Route::get('tasks/{id}', 'TasksController@show');
 // メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
@@ -25,9 +27,7 @@ Route::put('tasks/{id}', 'TasksController@update');
 Route::delete('tasks/{id}', 'TasksController@destroy');
 
 // index: showの補助ページ
-Route::get('tasks', 'TasksController@index');
-// create: 新規作成用のフォームページ
-Route::get('tasks/create', 'TasksController@create');
+Route::get('tasks', 'TasksController@index')->name('tasks.index');
 // edit: 更新用のフォームページ
 Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
 
